3f6765465e77d262cc350bbdfe6505d7bcfd4ffd
Olukyr <guilhermemotajau123@gmail.com>
2025-06-04 08:37:39 -0300

    dsds

--- script.js
+++
@@ -1,5 +0,0 @@
-const toggleBtn = document.getElementById('toggle-theme');
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
-  toggleBtn.textContent = document.body.classList.contains('dark-theme') ? '☀️' : '🌙';
-});
